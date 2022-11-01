056dd287b013159f2894367a06240e864b8eb0a9
Denise Neustadt <denise.neustadt@lemon.me>
2022-11-01 20:47:02 -0300

    first commit

---
+++ app/src/main/java/com/ort/edu/parcialtp3/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ort.edu.parcialtp3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
